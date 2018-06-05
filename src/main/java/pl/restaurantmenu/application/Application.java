309169edb1e7dbd75ed78e5334fb8cb695650eb6
Piotr <p_bartoszak@wp.pl>
2018-06-05 12:45:01 +0200

    Init application

---
+++ src/main/java/pl/restaurantmenu/application/Application.java
@@ -0,0 +1,12 @@
+package pl.restaurantmenu.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "pl.restaurantmenu")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
