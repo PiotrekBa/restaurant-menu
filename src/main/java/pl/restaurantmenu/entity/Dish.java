bd5cff98921ec225873c0cfc8a57d208bc252496
Piotr <p_bartoszak@wp.pl>
2018-06-05 13:08:40 +0200

    Add Restaurant, Menu and Dish classes

---
+++ src/main/java/pl/restaurantmenu/entity/Dish.java
@@ -0,0 +1,46 @@
+package pl.restaurantmenu.entity;
+
+public class Dish {
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private float price;
+
+    public Dish() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/pl/restaurantmenu/entity/Menu.java
@@ -0,0 +1,29 @@
+package pl.restaurantmenu.entity;
+
+import java.util.List;
+
+public class Menu {
+
+    private long id;
+
+    private List<Dish> dishes;
+
+    public Menu() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+}
